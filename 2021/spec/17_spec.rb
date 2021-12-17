985342b7acc0de86a6615531d889514ed5b1bfd0
Gene Hsu <gene@hsufarm.com>
2021-12-17 06:09:45 +0000

    Solve 2021 day 17

---
+++ 2021/spec/17_spec.rb
@@ -0,0 +1,41 @@
+require 'day17'
+
+RSpec.describe Day17 do
+  let (:example) { 'target area: x=20..30, y=-10..-5' }
+
+  context "Part 1" do
+    subject  { described_class.part1 input }
+
+    context "example" do
+      let(:input) { example }
+
+      it { is_expected.to eq 45 }
+    end
+
+    context "problem" do
+      let(:input) { day17 }
+      let(:answer) { 3160 }
+      it { is_expected.to eq answer }
+    end
+  end
+
+  context "Part 2" do
+    subject  { described_class.part2 input }
+
+    context "example" do
+      let(:input) { example }
+
+      it { is_expected.to eq 112 }
+    end
+
+    context "problem" do
+      let(:input) { day17 }
+      let(:answer) { 1928 }
+      it { is_expected.to eq answer }
+    end
+  end
+end
+
+def day17
+  'target area: x=282..314, y=-80..-45'
+end
